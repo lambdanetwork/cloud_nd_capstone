7811a20357c29f2467b769febecdabbbb4764d48
Vidy <vidy@pop-os.localdomain>
2020-11-21 16:30:22 +0700

    populate user.repository with dummy function

--- backend/src/repository/user.repository.ts
+++ backend/src/repository/user.repository.ts
@@ -18,27 +18,32 @@ interface IUserRepository {
 
 export const UserRepository: IUserRepository = class {
   static async getUploadPhotoUrl(imageId: string): Promise<string> {
-    return "";
+    return imageId;
   }
   static async updateUserType(
     userId: string,
     updateUser: UpdateUserTypeReq
   ): Promise<boolean> {
+    console.log(userId, updateUser);
     return false;
   }
   static async updateProfile(
     userId: string,
     updateUser: UpdateProfileReq
   ): Promise<boolean> {
+    console.log(userId, updateUser);
     return false;
   }
   static async getTodoByUserId(userId: string): Promise<User> {
+    console.log(userId);
     return;
   }
   static async create(newUser: User): Promise<User> {
-    return;
+    return newUser;
   }
   static async delete(userId: string): Promise<boolean> {
+    console.log(userId);
+
     return false;
   }
 };
