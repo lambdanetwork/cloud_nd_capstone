12dbe48d59c919cfbfa53b2bde630eddbaec3904
alfredov@garena.com <alfredov@garena.com>
2020-12-03 10:02:24 +0700

    1. CreateUserDTO, make optional 2. put back auth to createUser

--- backend/src/lambda/http/users/DTO/createUserDTO.ts
+++ backend/src/lambda/http/users/DTO/createUserDTO.ts
@@ -2,9 +2,14 @@ import { IsEmail, IsNotEmpty, IsOptional } from "class-validator";
 import { UserType } from "../../../../models/user.model";
 
 export class CreateUserDTO {
+  @IsOptional()
+  userId: string;
+
   // from auth0
-  @IsNotEmpty()
+  @IsOptional() @IsNotEmpty()
   username: string;
+
+  @IsOptional() 
   @IsEmail()
   @IsNotEmpty()
   emailMain: string;
@@ -16,10 +21,15 @@ export class CreateUserDTO {
   @IsOptional()
   phoneNumber?: string;
 
+  @IsOptional() 
   studentDetailId?: string;
+
+  @IsOptional() 
   tutorDetailId?: string;
+  
   @IsNotEmpty()
   type: UserType;
+  
   @IsOptional()
   photo?: string;
 }
